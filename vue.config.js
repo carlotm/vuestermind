71846eb764a03cb6b66ee2bd416baed165ea9e1c
Ascani Carlo <carlotm@protonmail.com>
2024-01-27 07:50:52 +0100

    Fix root path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
+
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vuestermind/'
+    : '/',
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
       args[0].title = "Vuestermind ✓ A master mind implementation in vue";
